38e141441fd8553e3d5d2ae7f7ea3da7d52a2f51
Jordi Duran i Batidor <joduba@iwith.org>
2018-02-08 21:58:05 +0100

    Now it’s working on the local environment with a local build, next step is to test to build with the pipeline.

--- imagine-universal/startup.sh
+++ imagine-universal/startup.sh
@@ -7,12 +7,16 @@
 #
 # In files where it can be found.
 
-FILES = `grep -l '%%___IMAGINE_APIURL___%%' /usr/html/dist/* /usr/html/dist-server* `
+FILES=`grep -l '%%___IMAGINE_APIURL___%%' /usr/html/dist/*.js /usr/html/dist-server/*.js`
 
-if [ ! -z "$FILES"];
+echo "ENV:---------IMAGINE_API_URL= [$IMAGINE_API_URL]----------"
+echo "ENV:---------IMAGINE_PAGE_URL=[$IMAGINE_PAGE_URL]---------"
+
+if [ ! -z "$FILES" ];
 then
     replace %%___IMAGINE_APIURL___%% $IMAGINE_API_URL $FILES
     replace %%___IMAGINE_PAGEURL___%% $IMAGINE_PAGE_URL $FILES
+    echo "++++++++++ Replace done"
 fi
 
 node server.js
